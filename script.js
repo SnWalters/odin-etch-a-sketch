b13c1a6934353f1d1654de2153a67698f031a93b
Sean <SWaltrs23@gmail.com>
2023-04-27 15:54:53 -0400

    Add functionality to color change button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const container = document.querySelector('#container');
+const screen = document.querySelector('#screen');
+const colors = ['blue', 'red', 'purple', 'black'];
+let colorCount = 1;
 let grid = prompt('What size grid would you like? (max 100)');
 
 if(grid > 100) {
@@ -14,7 +16,7 @@ for(let i = 0; i < grid; i++) {
         box.classList.add('box');
         line.appendChild(box);
     }
-    container.appendChild(line);
+    screen.appendChild(line);
 }
 
 const boxes = document.querySelectorAll('.box');
@@ -22,4 +24,15 @@ boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         box.classList.add('fill');
     });
+});
+
+const html = document.querySelector('html');
+const color = document.querySelector('#color');
+color.addEventListener('click', () => {
+    if(colorCount > colors.length - 1) {
+        colorCount = 0;
+    }
+    html.classList.remove(html.classList[0]);
+    html.classList.add(colors[colorCount]);
+    colorCount++;
 });
