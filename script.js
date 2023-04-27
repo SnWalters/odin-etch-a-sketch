06ab4c6d7bdcfed6a6f68dcb041b7d3a8773d89e
Sean <SWaltrs23@gmail.com>
2023-04-27 15:32:04 -0400

    Add ability for user to set grid size through prompt

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector('#container');
+let grid = prompt('What size grid would you like? (max 100)');
 
-for(let i = 0; i < 16; i++) {
+if(grid > 100) {
+    grid = 100;
+}
+console.log(grid);
+
+for(let i = 0; i < grid; i++) {
     const line = document.createElement('div');
     line.classList.add('row');
-    for(let j = 0; j < 16; j++) {
+    for(let j = 0; j < grid; j++) {
         const box = document.createElement('div');
         box.classList.add('box');
         line.appendChild(box);
